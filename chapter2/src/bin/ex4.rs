d91a1a6ac608d701725256020f05d1f6f99ed27c
kanarus <kanarus786@gmail.com>
2025-10-18 21:08:34 +0900

    how to collect iteration count smartly?

--- chapter2/src/bin/ex4.rs
+++ chapter2/src/bin/ex4.rs
@@ -2,7 +2,29 @@ use chapter2::{EPSILON, FirstEigenvalueSolver, EigenvalueErrors};
 use nalgebra::{SMatrix, SVector};
 
 fn solve_by_power_iteration<const N: usize>(a: SMatrix<f64, N, N>) -> (f64, SVector<f64, N>) {
-    todo!()
+    const MAX_ITERATIONS: usize = 100_000;
+    
+    let mut mu = Vec::<f64>::new();
+    let mut x_k = SVector::<f64, N>::from_fn(|_, _| 1.0);
+    for _ in 0..MAX_ITERATIONS {
+        let y_k = a * x_k;
+        
+        let (i, _max_abs) = x_k
+            .iter()
+            .enumerate()
+            .max_by(|(_, p), (_, q)| f64::partial_cmp(&p.abs(), &q.abs()).expect("found NaN or Inf"))
+            .expect("Vector is zero");
+        
+        let mu_k = y_k[i] / x_k[i];
+        if mu.last().is_some_and(|mu_prev| (mu_k - mu_prev).abs() < EPSILON) {
+            return (mu_k, x_k);
+        }
+        
+        x_k = y_k / y_k.norm();
+        mu.push(mu_k);
+    }
+    
+    panic!("`mu` seems to diverge");
 }
 
 fn experiment<const N: usize>() -> chapter2::ExperimentResult<(f64, SVector<f64, N>), EigenvalueErrors> {
